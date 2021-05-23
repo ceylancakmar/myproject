733a9b09b9503f553887ede57162581494391901
ceylancakmar <cakmarceylan@gmail.com>
2021-05-24 02:18:17 +0300

    mvc

---
+++ my_project/Controllers/hakkımdaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_project.Controllers
+{
+    public class hakkımdaController : Controller
+    {
+        // GET: hakkımda
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
